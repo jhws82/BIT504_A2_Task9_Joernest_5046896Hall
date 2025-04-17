18a096729f2b9e81d355d6044dac3acea503340a
joeha <joeha@LAPTOP-V829M0ER.lan>
2025-04-17 20:02:56 +1000

    Initial commit of project.

---
+++ BIT504_A2_Task9_JoernestHall_5046896/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Welcome to the Sales Data Program!");
+		System.out.println("This program will display sales data for the year.");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
---
+++ BIT504_A2_Task9_JoernestHall_5046896/src/SalesData.java
@@ -0,0 +1,10 @@
+
+public class SalesData {
+	private int data[] = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+		
+	}
+
+}
